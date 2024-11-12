b15ad381d0a3788f769e66dfca0ac6454932926c
xelisce <xelisce@gmail.com>
2024-11-12 15:35:14 +0800

    motors accel

--- motors/motors.ino
+++ motors/motors.ino
@@ -1,96 +1,66 @@
-
 #include "MeMCore.h"
 #define LEFT_MOTOR_BIAS 1              // from 0 to 1, because robot doesn't move straight
-#define RIGHT_MOTOR_BIAS 0.75             // from 0 to 1, because robot doesn't move straight
+#define RIGHT_MOTOR_BIAS 0.75          // from 0 to 1, because robot doesn't move straight
 #define MOTOR_BIAS_MORE_RIGHT 0.77
 
 MeDCMotor leftMotor(M1);  // assigning leftMotor to port M1
 MeDCMotor rightMotor(M2); // assigning RightMotor to port M2
-uint8_t motorSpeed = 180;
+uint8_t motorSpeed = 255;
 
+// Function to gradually accelerate or decelerate
+void gradualSpeedTurn(MeDCMotor *leftMotor, MeDCMotor *rightMotor, int targetSpeed, int stepDelay, bool isRightTurn)
+{
+  static int currentSpeed = 0; // Keeps track of the current speed
+  
+  // Gradually accelerate or decelerate to targetSpeed
+  while (currentSpeed != targetSpeed)
+  {
+    if (currentSpeed < targetSpeed)
+      currentSpeed++;
+    else if (currentSpeed > targetSpeed)
+      currentSpeed--;
+
+    // Adjust motor direction and speed for turning
+    if (isRightTurn) {
+      rightMotor->run(-currentSpeed * RIGHT_MOTOR_BIAS);
+      leftMotor->run(-currentSpeed * LEFT_MOTOR_BIAS);
+    } else {
+      rightMotor->run(currentSpeed * RIGHT_MOTOR_BIAS);
+      leftMotor->run(currentSpeed * LEFT_MOTOR_BIAS);
+    }
+
+    delayMicroseconds(stepDelay);
+  }
+}
 
 void turnLeftBlocking(MeDCMotor *leftMotor, MeDCMotor *rightMotor)
 {
-  rightMotor->run(motorSpeed * RIGHT_MOTOR_BIAS);
-  leftMotor->run(motorSpeed * LEFT_MOTOR_BIAS);
-  delay(570);
-  leftMotor->stop();
-  rightMotor->stop();
-}
-
-void turnLeftUTurnBlocking(MeDCMotor *leftMotor, MeDCMotor *rightMotor)
-{
-  turnLeftBlocking(leftMotor, rightMotor);
-  delay(500);
-  moveStraightBlocking(leftMotor, rightMotor, 1000);
-  delay(500);
-  turnLeftBlocking(leftMotor, rightMotor);
-  delay(500);
-  leftMotor->stop();
-  rightMotor->stop();
+  // Accelerate to target speed
+  gradualSpeedTurn(leftMotor, rightMotor, motorSpeed, 400, false);
+  delay(295); // Maintain full speed for the duration of the turn
+  // Decelerate to stop
+  gradualSpeedTurn(leftMotor, rightMotor, 0, 400, false);
 }
 
 void turnRightBlocking(MeDCMotor *leftMotor, MeDCMotor *rightMotor)
 {
-  rightMotor->run(-motorSpeed * RIGHT_MOTOR_BIAS);
-  leftMotor->run(-motorSpeed * LEFT_MOTOR_BIAS);
-  delay(570);
-  leftMotor->stop();
-  rightMotor->stop();
+  // Accelerate to target speed
+  gradualSpeedTurn(leftMotor, rightMotor, motorSpeed, 400, true);
+  delay(295); // Maintain full speed for the duration of the turn
+  // Decelerate to stop
+  gradualSpeedTurn(leftMotor, rightMotor, 0, 400, true);
 }
 
-void turnRightUTurnBlocking(MeDCMotor *leftMotor, MeDCMotor *rightMotor)
-{
-  turnRightBlocking(leftMotor, rightMotor);
-  delay(500);
-  moveStraightBlocking(leftMotor, rightMotor, 1000);
-  delay(500);
-  turnRightBlocking(leftMotor, rightMotor);
-  delay(500);
-  leftMotor->stop();
-  rightMotor->stop();
-}
-
-void turnOnTheSpotBlocking(MeDCMotor *leftMotor, MeDCMotor *rightMotor)
-{
-  rightMotor->run(-motorSpeed * RIGHT_MOTOR_BIAS);
-  leftMotor->run(-motorSpeed * LEFT_MOTOR_BIAS);
-  delay(1135);
-  leftMotor->stop();
-  rightMotor->stop();
-}
-
-void moveStraightBlocking(MeDCMotor *leftMotor, MeDCMotor *rightMotor, int time)
-{
-  rightMotor->run(motorSpeed * MOTOR_BIAS_MORE_RIGHT);
-  leftMotor->run(-motorSpeed * LEFT_MOTOR_BIAS);
-  delay(time);
-  leftMotor->stop();
-  rightMotor->stop();
-}
-
-void moveStraight(MeDCMotor *leftMotor, MeDCMotor *rightMotor)
-{
-  rightMotor->run(motorSpeed * RIGHT_MOTOR_BIAS);
-  leftMotor->run(-motorSpeed * LEFT_MOTOR_BIAS);
-}
-
-void stopMotors(MeDCMotor *leftMotor, MeDCMotor *rightMotor)
-{
-  leftMotor->stop();
-  rightMotor->stop();
-}
-
-// Setting motor speed to an integer between 1 and 255
-// The larger the number, the faster the speed
 void setup()
 {
-  // Any setup code here runs only once:
-  delay(1000); // Do nothing for 10000 ms = 10 seconds
+  delay(1000); // Initial delay before starting
 }
+
 void loop()
 {
-  // turnRightBlocking(&leftMotor, &rightMotor);
   delay(1000);
-  turnOnTheSpotBlocking(&leftMotor, &rightMotor);
+  turnLeftBlocking(&leftMotor, &rightMotor); // Turn left with acceleration and deceleration
+  delay(1000);
+  turnRightBlocking(&leftMotor, &rightMotor); // Turn right with acceleration and deceleration
+  delay(1000);
 }
