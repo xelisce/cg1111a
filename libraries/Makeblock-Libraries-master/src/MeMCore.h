5b03da3ce1836c2912496018a3f7ad9c2b17dfe5
xelisce <xelisce@gmail.com>
2024-11-13 17:23:06 +0800

    Reverted library of mbot to original

--- libraries/Makeblock-Libraries-master/src/MeMCore.h
+++ libraries/Makeblock-Libraries-master/src/MeMCore.h
@@ -74,6 +74,12 @@
 #include "MePS2.h"
 #include "MeColorSensor.h"
 /*********************  Mbot Board GPIO Map *********************************/
-extern MePort_Sig mePort[17]; //! self changed
+MePort_Sig mePort[17] =
+{
+  { NC, NC }, { 11, 12 }, {  9, 10 }, { A2, A3 }, { A0, A1 },
+  { NC, NC }, {  8, A6 }, { A7, 13 }, {  8, A6 }, {  6,  7 },
+  {  5,  4 }, { NC, NC }, { NC, NC }, { NC, NC }, { NC, NC },
+  { NC, NC }, { NC, NC }
+};
 #endif // MeMCore_H
 
